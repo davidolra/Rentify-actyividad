3087bbc60501aea4c62b32e1fd10209ead3d2ae6
davidolra <da.olaver@duocuc.cl>
2025-09-24 15:24:52 -0300

    erg

---
+++ rentify_grupo1/app/src/main/java/com/example/rentify_grupo1/ui/screen/HomeScreen.kt
@@ -0,0 +1,53 @@
+package com.example.rentify_grupo1.ui.screen
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.rentify_grupo1.R
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen() {
+    Scaffold(
+        topBar = {
+            TopAppBar(title = { Text("Mi App Kotlin") })
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(20.dp)
+        ) {
+            Text(text = "¡Bienvenido!")
+            Button(onClick = { /* acción futura */ }) {
+                Text("Presióname")
+            }
+            Image(
+                painter = painterResource(id = R.drawable.sdfaerg),
+                contentDescription = "Logo App",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(150.dp),
+                contentScale = ContentScale.Fit
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomeScreenPreview(){
+    HomeScreen()
+}
